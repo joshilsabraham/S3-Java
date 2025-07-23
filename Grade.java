43de6ef7ad9ab9411464869f52a1b046416b7bcb
Joshil S Abraham <80256698+joshilsabraham@users.noreply.github.com>
2025-07-23 09:04:34 +0500

    Update Grade.java

--- Grade.java
+++ Grade.java
@@ -2,27 +2,36 @@ package jxh;
 
 import java.util.Scanner;
 
-class Student{
-	int english;
-	int maths;
-	int science;
-	public static void inputMarks() {
-		Scanner sc=new Scanner(System.in);
-		english=sc.nextInt();
-		maths=sc.nextInt();
-		science=sc.nextInt();
-	}
-	public static void calculate() {
-		int marks= (english+maths+science)/3;
-		System.out.println(marks);
-	}
+class Student {
+    int english;
+    int maths;
+    int science;
+
+    public void inputMarks() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter English marks: ");
+        english = sc.nextInt();
+        System.out.print("Enter Maths marks: ");
+        maths = sc.nextInt();
+        System.out.print("Enter Science marks: ");
+        science = sc.nextInt();
+    }
+
+    public void calculate() {
+        int marks = (english + maths + science) / 3;
+        System.out.println("Average marks: " + marks);
+    }
 }
 
 public class Grade {
-	public static void main(String[]args) {		
-		System.out.println("Enter Marks: ");
-		Student s1=new Student();
-		Student.inputMarks();
-		Student.calculate();		
-	}
+    public static void main(String[] args) {
+        System.out.println("Enter Marks");
+        Student s1 = new Student();
+        s1.inputMarks();
+        s1.calculate();
+        
+        
+        
+        
+    }
 }
