ac36126e64866591755507219993d7d17b112e75
Joshil S Abraham <80256698+joshilsabraham@users.noreply.github.com>
2025-07-23 09:20:15 +0500

    Create Days.java

---
+++ Days.java
@@ -0,0 +1,36 @@
+package jxh;
+
+import java.util.Scanner;
+
+public class Days {
+	 public static void main(String[] args) {
+		 int day;
+		 Scanner sc = new Scanner(System.in);
+	     System.out.print("Enter An Integer(1-7): ");
+	     day = sc.nextInt();
+		 System.out.print("The Corresponding Day is " );
+         switch (day) {
+         case 1:
+           System.out.print("Monday");
+           break;
+         case 2:
+           System.out.print("Tuesday");
+           break;
+         case 3:
+           System.out.print("Wednesday");
+           break;
+         case 4:
+           System.out.print("Thursday");
+           break;
+         case 5:
+           System.out.print("Friday");
+           break;
+         case 6:
+           System.out.print("Saturday");
+           break;
+         case 7:
+           System.out.print("Sunday");
+	       break;
+	     }
+	 }    
+}
