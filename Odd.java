bfd54bb345ae3b7f739415762959fd1c859a08a2
Joshil S Abraham <80256698+joshilsabraham@users.noreply.github.com>
2025-07-23 09:29:55 +0500

    Update Odd.java

--- Odd.java
+++ Odd.java
@@ -1,12 +1,10 @@
-public class Odd() {
-	public static void main(String[]args) {
-	for(int i=1;i<50;i++) {
-		if(i%2==0) {
-			continue;
-		}
-		else {
-		System.out.println(i);
-		}
-	}
-	}
+public class Odd {
+    public static void main(String[] args) {
+        for (int i = 1; i < 50; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
 }
